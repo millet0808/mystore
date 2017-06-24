f180496207d9360ae7824b2e75ca0d22f20f6dfc
Millet Hsieh <millet@amt.tw>
2017-06-24 22:35:25 +0800

    Add user manager

--- estore/views.py
+++ estore/views.py
@@ -1,17 +1,12 @@
 from django.contrib import messages
-# from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.models import Group, User
 from django.urls import reverse
 from django.views import generic
-# from django.contrib.auth.decorators import permission_required
 from .models import Product
-# from .forms import ProductForm
 
 
 # Create your views here.
-# def product_list(request):
-#     products = Product.objects.all()
-#     return render(request, 'estore/product_index.html', {'products': products})
 class ProductList(PermissionRequiredMixin, generic.ListView):
     model = Product
 
@@ -25,16 +20,7 @@ class ProductList(PermissionRequiredMixin, generic.ListView):
 class ProductDetail(generic.DetailView):
     model = Product
 
-# @permission_required('estore.add_product')
-# def product_create(request):
-#     if request.method == "POST":
-#         form = ProductForm(request.POST)
-#         if form.is_valid():
-#             product = form.save()
-#             return redirect('product_index')
-#     else:
-#         form = ProductForm()
-#     return render(request, 'estore/product_new.html', {'form': form})
+
 class ProductCreate(PermissionRequiredMixin, generic.CreateView):
     permission_required = 'estore.add_product'
     model = Product
@@ -45,16 +31,6 @@ class ProductCreate(PermissionRequiredMixin, generic.CreateView):
         return reverse('dashboard_product_list')
 
 
-# @permission_required('estore.change_product')
-# def product_update(request, pk):
-#     product = get_object_or_404(Product, pk=pk)
-#     form = ProductForm(request.POST or None, instance=product)
-#
-#     if request.POST and form.is_valid():
-#         form.save()
-#         messages.success(request, '產品已變更')
-#
-#     return render(request, 'estore/product_form.html', {'form': form})
 class ProductUpdate(PermissionRequiredMixin, generic.UpdateView):
     permission_required = 'estore.change_product'
     model = Product
@@ -63,3 +39,35 @@ class ProductUpdate(PermissionRequiredMixin, generic.UpdateView):
     def get_success_url(self):
         messages.success(self.request, '產品已變更')
         return reverse('dashboard_product_update', kwargs=self.kwargs)
+
+
+class UserList(PermissionRequiredMixin, generic.ListView):
+    permission_required = 'auth.change_user'
+    model = User
+    template_name = 'estore/dashboard_user_list.html'
+
+
+class UserAddToStaff(PermissionRequiredMixin, generic.UpdateView):
+    permission_required = 'auth.change_user'
+    model = User
+    fields = []
+
+    def get_success_url(self):
+        if self.request.method == 'POST':
+            group = Group.objects.get(name='estore_staff')
+            group.user_set.add(self.object)
+            messages.success(self.request, '已變更使用者身份為管理者')
+        return reverse('dashboard_user_list')
+
+
+class UserRemoveFromStaff(PermissionRequiredMixin, generic.UpdateView):
+    permission_required = 'auth.change_user'
+    model = User
+    fields = []
+
+    def get_success_url(self):
+        if self.request.method == 'POST':
+            group = Group.objects.get(name='estore_staff')
+            group.user_set.remove(self.object)
+            messages.success(self.request, '已變更使用者身份為一般使用者')
+        return reverse('dashboard_user_list')
