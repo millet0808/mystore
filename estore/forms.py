eb2e113c0c30529c35668a7280f34a77934944f4
Millet Hsieh <millet@amt.tw>
2017-07-14 22:10:07 +0800

    Implement order create

--- estore/forms.py
+++ estore/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from .models import Product
+from .models import OrderInfo
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ('title', 'description', 'quantity', 'price')
+
+
+class OrderInfoForm(forms.ModelForm):
+    class Meta:
+        model = OrderInfo
+        fields = ['billing_name', 'billing_address', 'shipping_name', 'shipping_address']
--- estore/models.py
+++ estore/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
+
 
 # Create your models here.
 class Product(models.Model):
@@ -21,3 +23,23 @@ class Cart(models.Model):
         for product in self.items.all():
             sum += product.price
         return sum
+
+
+class OrderInfo(models.Model):
+    billing_name = models.CharField(max_length=255, verbose_name='付款人姓名')
+    billing_address = models.CharField(max_length=255, verbose_name='帳單地址')
+    shipping_name = models.CharField(max_length=255, verbose_name='收件人姓名')
+    shipping_address = models.CharField(max_length=255, verbose_name='收件地址')
+
+
+class Order(models.Model):
+    info = models.OneToOneField(OrderInfo, on_delete=models.CASCADE, primary_key=True)
+    total = models.IntegerField(default=0, verbose_name='訂單總金額')
+    user = models.ForeignKey(User)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order)
+    title = models.CharField(max_length=255, verbose_name='產品名稱')
+    price = models.IntegerField(max_length=255, verbose_name='價格')
+    quantity = models.IntegerField(max_length=255, verbose_name='數量')
